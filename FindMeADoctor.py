58333b04505eb1f65964a9610cb8b4d2c5bfc62d
Akshat <akshatm408@gmail.com>
2021-10-27 13:19:49 +0530

    Completed Dictionary

--- FindMeADoctor.py
+++ FindMeADoctor.py
@@ -28,22 +28,36 @@ def token_input():
         else:
             print("Please Enter A Valid Input");
 
-
-print("What Kind of Doctor Are you Looking for?\nChoose From The List Below!\n")
-docdict = {0:"NeuroSurgeon",1:"Dentist",2:"Orthopaedic",3:"Physician",4:"Cardiologist",5:"",7:"",8:"",9:"Something Else"}
-intake = dictionary_input()
-if int(intake)==9:
-    print("You chose Others")
-    print("Enter Keywords Separated By Commas (,)");
-    intake = (token_input()+' near me').replace(',','%2C').replace(' ','+')
-    driver.get('https://www.google.com/search?q='+intake)
-    driver.find_element(By.CLASS_NAME,"wUrVib").click()
-    items = driver.find_elements_by_xpath("//*[contains(@id, 'tsuid')]")
-    for item in items:
-        print(item.text)
-        print("\n")
+while True:
+    print("Welcome To The Doctor Finder\n")
+    print("What Kind of Doctor Are you Looking for?\nChoose From The List Below!\n")
+    print("Type 'ESC' and press Enter at any point in the program , to exit\n ")
+    docdict = {0:"NeuroSurgeon",1:"Dentist",2:"Orthopaedic",3:"Physician",4:"Cardiologist",5:"Pediatrician",7:"Psychiatrist",8:"Rheumatologist",9:"Something Else"}
+    intake = dictionary_input()
+    if intake=='ESC':
+        break;
     
-print(intake)
+    elif int(intake)==9:
+        print("You chose Others")
+        print("Enter Keywords Separated By Commas (,)");
+        intake = (token_input()+' near me').replace(',','%2C').replace(' ','+')
+        driver.get('https://www.google.com/search?q='+intake)
+        driver.find_element(By.CLASS_NAME,"wUrVib").click()
+        items = driver.find_elements_by_xpath("//*[contains(@id, 'tsuid')]")
+        for item in items:
+            print(item.text)
+            print("\n")
+    else:
+        intake=docdict[int(intake)]
+        driver.get('https://www.google.com/search?q='+intake)
+        driver.find_element(By.CLASS_NAME,"wUrVib").click()
+        items = driver.find_elements_by_xpath("//*[contains(@id, 'tsuid')]")
+        for item in items:
+            print(item.text)
+            print("\n")
+        
+    print(intake)
+
 
 
 
